d3f38d2c5c59b16d7ff28f5ba92d8118f5a7deb2
mzz2017 <m@mzz.pub>
2018-06-16 17:42:33 +0800

    chore(dataSource): 更换dataSource为api.mzz.pub服务器数据库

--- dao/dao.go
+++ dao/dao.go
@@ -5,6 +5,9 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"sync"
+	"io/ioutil"
+	"github.com/tidwall/gjson"
+	"fmt"
 )
 
 var db *gorm.DB
@@ -12,7 +15,14 @@ var once sync.Once
 
 func initDB() {
 	var err error
-	db, err = gorm.Open("mysql", "root:root@/city_lianjia_data?parseTime=true")
+	cnf, err := ioutil.ReadFile("conf/app.json")
+	if err != nil {
+		panic("数据库配置文件读取失败。请将app_reference.json复制为app.json后，修改app.json中的配置")
+	}
+	print(cnf)
+	results := gjson.GetManyBytes(cnf, "dataSource.host", "dataSource.port", "dataSource.database", "dataSource.username", "dataSource.password")
+	args := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true", results[3].String(), results[4].String(), results[0].String(), results[1].Int(), results[2].String())
+	db, err = gorm.Open("mysql", args)
 	if err != nil {
 		log.Fatal(err)
 	}
